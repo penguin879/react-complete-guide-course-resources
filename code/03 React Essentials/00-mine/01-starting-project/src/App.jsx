9ff33560f88bc74c3a1daa75d7f71eccc5862730
penguin879 <prestondulion@gmail.com>
2025-02-26 21:10:16 +0000

    alternative to pulling out props

--- code/03 React Essentials/00-mine/01-starting-project/src/App.jsx
+++ code/03 React Essentials/00-mine/01-starting-project/src/App.jsx
@@ -42,11 +42,7 @@ function App() {
         <section id="core-concepts">
           <h2>Time to get started!</h2>
           <ul>
-            <CoreConcept
-              title={CORE_CONCEPTS[0].title}
-              description={CORE_CONCEPTS[0].description}
-              image={CORE_CONCEPTS[0].image}
-            />
+            <CoreConcept {...CORE_CONCEPTS[0]}/>
             <CoreConcept
               title={CORE_CONCEPTS[1].title}
               description={CORE_CONCEPTS[1].description}
