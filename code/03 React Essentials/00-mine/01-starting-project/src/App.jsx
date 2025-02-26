01caccac8f2f92f5cc393f7084fc748e88d29a72
penguin879 <prestondulion@gmail.com>
2025-02-26 21:57:09 +0000

    anon function in button

--- code/03 React Essentials/00-mine/01-starting-project/src/App.jsx
+++ code/03 React Essentials/00-mine/01-starting-project/src/App.jsx
@@ -4,8 +4,10 @@ import CoreConcept from "./components/CoreConcept.jsx";
 import TabButton from "./components/TabButton.jsx";
 
 function App() {
-  function handleClick() {
-    console.log("Hello World! - selected");
+  function handleClick(selectedButton) {
+    // selectedButton => 'components', 'jsx', 'props', 'state'
+
+    console.log(selectedButton);
   }
 
   return (
@@ -24,10 +26,12 @@ function App() {
         <section id="examples">
           <h2>Examples</h2>
           <menu>
-            <TabButton onSelect={handleClick}>Components</TabButton>
-            <TabButton onSelect={handleClick}>JSX</TabButton>
-            <TabButton onSelect={handleClick}>Props</TabButton>
-            <TabButton onSelect={handleClick}>State</TabButton>
+            <TabButton onSelect={() => handleClick("component")}>
+              Components
+            </TabButton>
+            <TabButton onSelect={() => handleClick("jsx")}>JSX</TabButton>
+            <TabButton onSelect={() => handleClick("props")}>Props</TabButton>
+            <TabButton onSelect={() => handleClick("state")}>State</TabButton>
           </menu>
           Dynamic Content
         </section>
