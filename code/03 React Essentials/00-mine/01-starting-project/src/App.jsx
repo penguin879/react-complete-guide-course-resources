f59cd78336dc0100832bfa1333ee5819bc8c5a3b
penguin879 <prestondulion@gmail.com>
2025-02-26 21:53:30 +0000

    move click handler so can interact with dynamic content

--- code/03 React Essentials/00-mine/01-starting-project/src/App.jsx
+++ code/03 React Essentials/00-mine/01-starting-project/src/App.jsx
@@ -4,6 +4,10 @@ import CoreConcept from "./components/CoreConcept.jsx";
 import TabButton from "./components/TabButton.jsx";
 
 function App() {
+  function handleClick() {
+    console.log("Hello World! - selected");
+  }
+
   return (
     <div>
       <Header />
@@ -20,11 +24,12 @@ function App() {
         <section id="examples">
           <h2>Examples</h2>
           <menu>
-            <TabButton>Components</TabButton>
-            <TabButton>JSX</TabButton>
-            <TabButton>Props</TabButton>
-            <TabButton>State</TabButton>
+            <TabButton onSelect={handleClick}>Components</TabButton>
+            <TabButton onSelect={handleClick}>JSX</TabButton>
+            <TabButton onSelect={handleClick}>Props</TabButton>
+            <TabButton onSelect={handleClick}>State</TabButton>
           </menu>
+          Dynamic Content
         </section>
       </main>
     </div>
