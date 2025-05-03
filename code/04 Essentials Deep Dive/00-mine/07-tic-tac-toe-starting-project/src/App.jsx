77c0e7b02a8a7346104ab087e887c8a681b24dd7
penguin879 <prestondulion@gmail.com>
2025-05-03 19:59:10 +0000

    fix player order

--- code/04 Essentials Deep Dive/00-mine/07-tic-tac-toe-starting-project/src/App.jsx
+++ code/04 Essentials Deep Dive/00-mine/07-tic-tac-toe-starting-project/src/App.jsx
@@ -12,8 +12,8 @@ function App() {
     <main>
       <div id="game-container">
         <ol id="players" className="highlight-player">
-          <Player initialName="Player 2" symbol="O" isActive={activePlayer === "X"}/>
-          <Player initialName="Player 1" symbol="X" isActive={activePlayer === "O"}/>
+          <Player initialName="Player 1" symbol="X" isActive={activePlayer === "X"}/>
+          <Player initialName="Player 2" symbol="O" isActive={activePlayer === "O"}/>
         </ol>
         <GameBoard onSelectSquare={handleSelectSquare} activePlayerSymbol={activePlayer} />
       </div>
