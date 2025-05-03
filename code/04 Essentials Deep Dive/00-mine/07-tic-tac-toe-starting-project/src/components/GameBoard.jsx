5c927f8015a665065d890f85cc6826721ed65f02
penguin879 <prestondulion@gmail.com>
2025-05-03 20:18:04 +0000

    re add rowIndex & colIndex

--- code/04 Essentials Deep Dive/00-mine/07-tic-tac-toe-starting-project/src/components/GameBoard.jsx
+++ code/04 Essentials Deep Dive/00-mine/07-tic-tac-toe-starting-project/src/components/GameBoard.jsx
@@ -11,7 +11,7 @@ export default function GameBoard({ onSelectSquare, turns }) {
   for (const turn of turns) {
     const { square, player } = turn;
     const { row, col } = square;
-    
+
     gameBoard[row][col] = player;
   }
 
@@ -36,7 +36,7 @@ export default function GameBoard({ onSelectSquare, turns }) {
           <ol>
             {row.map((playerSymbol, colIndex) => (
               <li key={colIndex}>
-                <button onClick={onSelectSquare}>{playerSymbol}</button>
+                <button onClick={() => {onSelectSquare(rowIndex, colIndex)}}>{playerSymbol}</button>
               </li>
             ))}
           </ol>
