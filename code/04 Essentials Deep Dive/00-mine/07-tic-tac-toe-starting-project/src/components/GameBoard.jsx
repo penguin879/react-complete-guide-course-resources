cb7544eb3974f04c746463c8bc677e7e716b734e
penguin879 <prestondulion@gmail.com>
2025-05-03 19:39:02 +0000

    initial gameboard

--- code/04 Essentials Deep Dive/00-mine/07-tic-tac-toe-starting-project/src/components/GameBoard.jsx
+++ code/04 Essentials Deep Dive/00-mine/07-tic-tac-toe-starting-project/src/components/GameBoard.jsx
@@ -8,7 +8,15 @@ export default function GameBoard() {
   return (
     <ol id="game-board">
       {initialGameBoard.map((row, rowIndex) => (
-        <li key={rowIndex}></li>
+        <li key={rowIndex}>
+          <ol>
+            {row.map((playerSymbol, colIndex) => (
+              <li key={colIndex}>
+                <button>{playerSymbol}</button>
+              </li>
+            ))}
+          </ol>
+        </li>
       ))}
     </ol>
   );
