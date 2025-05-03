770a4a613b170338972a04d530f165d30cc6ac32
penguin879 <prestondulion@gmail.com>
2025-05-03 19:19:51 +0000

    update player name

--- code/04 Essentials Deep Dive/00-mine/07-tic-tac-toe-starting-project/src/components/Player.jsx
+++ code/04 Essentials Deep Dive/00-mine/07-tic-tac-toe-starting-project/src/components/Player.jsx
@@ -1,24 +1,37 @@
 import { useState } from "react";
 
-export default function Player({ name, symbol }) {
+export default function Player({ initialName, symbol }) {
   const [isEditing, setIsEditing] = useState(false);
+  const [playerName, setPlayerName] = useState(initialName);
 
   function handleEditClick() {
     setIsEditing((editing) => !editing);
   }
 
-  let playerName = <span className="player-name">{name}</span>;
+  function handleChange(event) {
+    setPlayerName(event.target.value);
+  }
+
+  let editablePlayerName = <span className="player-name">{playerName}</span>;
   let btnCaption = "Edit";
 
   if (isEditing) {
-    playerName = <input type="text" placeholder={name} value={name} required />;
+    editablePlayerName = (
+      <input
+        type="text"
+        placeholder={playerName}
+        value={playerName}
+        onChange={handleChange}
+        required
+      />
+    );
     btnCaption = "Save";
   }
 
   return (
     <li>
       <span className="player">
-        {playerName}
+        {editablePlayerName}
         <span className="player-symbol">{symbol}</span>
         <button onClick={handleEditClick}>{btnCaption}</button>
       </span>
