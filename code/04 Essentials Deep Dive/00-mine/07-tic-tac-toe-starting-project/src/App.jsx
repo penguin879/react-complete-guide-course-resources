e6dd4f0add7e8aca3ed8c183d439cc5e05bd19aa
penguin879 <prestondulion@gmail.com>
2025-05-03 20:48:57 +0000

    variable typo

--- code/04 Essentials Deep Dive/00-mine/07-tic-tac-toe-starting-project/src/App.jsx
+++ code/04 Essentials Deep Dive/00-mine/07-tic-tac-toe-starting-project/src/App.jsx
@@ -32,10 +32,10 @@ function App() {
   }
   let winner;
   for (const combination of WINNING_COMBINATIONS) {
-    const firstSquareSymbol = gameBoard[combination[0].row][combination[0].col];
+    const firstSquareSymbol = gameBoard[combination[0].row][combination[0].column];
     const secondSquareSymbol =
-      gameBoard[combination[1].row][combination[1].col];
-    const thirdSquareSymbol = gameBoard[combination[2].row][combination[2].col];
+      gameBoard[combination[1].row][combination[1].column];
+    const thirdSquareSymbol = gameBoard[combination[2].row][combination[2].column];
 
     if (
       firstSquareSymbol &&
