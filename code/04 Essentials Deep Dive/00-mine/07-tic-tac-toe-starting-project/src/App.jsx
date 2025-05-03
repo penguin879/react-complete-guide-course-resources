f903a37d2335a625d0c38ff5d3979e22aa23fb2f
penguin879 <prestondulion@gmail.com>
2025-05-03 20:01:35 +0000

    log init

--- code/04 Essentials Deep Dive/00-mine/07-tic-tac-toe-starting-project/src/App.jsx
+++ code/04 Essentials Deep Dive/00-mine/07-tic-tac-toe-starting-project/src/App.jsx
@@ -1,5 +1,6 @@
 import Player from "./components/Player.jsx";
 import GameBoard from "./components/GameBoard.jsx";
+import Log from "./components/Log.jsx";
 import { useState } from "react";
 function App() {
   const [activePlayer, setActivePlayer] = useState("X");
@@ -12,10 +13,21 @@ function App() {
     <main>
       <div id="game-container">
         <ol id="players" className="highlight-player">
-          <Player initialName="Player 1" symbol="X" isActive={activePlayer === "X"}/>
-          <Player initialName="Player 2" symbol="O" isActive={activePlayer === "O"}/>
+          <Player
+            initialName="Player 1"
+            symbol="X"
+            isActive={activePlayer === "X"}
+          />
+          <Player
+            initialName="Player 2"
+            symbol="O"
+            isActive={activePlayer === "O"}
+          />
         </ol>
-        <GameBoard onSelectSquare={handleSelectSquare} activePlayerSymbol={activePlayer} />
+        <GameBoard
+          onSelectSquare={handleSelectSquare}
+          activePlayerSymbol={activePlayer}
+        />
       </div>
       LOG
     </main>
