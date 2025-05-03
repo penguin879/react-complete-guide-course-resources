fa14f9d6f00944a0a5210f8084a073d814c0a396
penguin879 <prestondulion@gmail.com>
2025-05-03 19:58:28 +0000

    X & O can play

--- code/04 Essentials Deep Dive/00-mine/07-tic-tac-toe-starting-project/src/App.jsx
+++ code/04 Essentials Deep Dive/00-mine/07-tic-tac-toe-starting-project/src/App.jsx
@@ -11,11 +11,11 @@ function App() {
   return (
     <main>
       <div id="game-container">
-        <ol id="players">
-          <Player initialName="Player 1" symbol="X" />
-          <Player initialName="Player 2" symbol="O" />
+        <ol id="players" className="highlight-player">
+          <Player initialName="Player 2" symbol="O" isActive={activePlayer === "X"}/>
+          <Player initialName="Player 1" symbol="X" isActive={activePlayer === "O"}/>
         </ol>
-        <GameBoard />
+        <GameBoard onSelectSquare={handleSelectSquare} activePlayerSymbol={activePlayer} />
       </div>
       LOG
     </main>
