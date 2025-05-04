d47935f25a5ddd9d7789dcc99ac41181f0be5063
penguin879 <prestondulion@gmail.com>
2025-05-04 22:21:11 +0000

    typecast

--- code/05 Essentials Practice/00-mine/01-starting-project/src/App.jsx
+++ code/05 Essentials Practice/00-mine/01-starting-project/src/App.jsx
@@ -14,7 +14,7 @@ function App() {
     setUserInput((prevUserInput) => {
       return {
         ...prevUserInput,
-        [inputIdentifier]: newValue,
+        [inputIdentifier]: +newValue,
       };
     });
   }
