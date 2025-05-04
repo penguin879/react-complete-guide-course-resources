254fd35da2dcff4eeb704ffc9f5d41c3f11cc2b3
penguin879 <prestondulion@gmail.com>
2025-05-04 20:18:58 +0000

    add header

---
+++ code/05 Essentials Practice/00-mine/01-starting-project/src/components/Header.jsx
@@ -0,0 +1,8 @@
+import money from '../assets/investment-calculator-logo.png'
+
+export default function Header() {
+    return <header id="header">
+        <img src={money} alt="Green money bag" />
+        <h1>Investment Calculator</h1>
+    </header>
+}
