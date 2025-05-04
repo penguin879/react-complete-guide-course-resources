ac0a094b2126885d8962468d07b436be44dd106a
penguin879 <prestondulion@gmail.com>
2025-05-04 22:34:24 +0000

    complete

--- code/05 Essentials Practice/00-mine/01-starting-project/src/components/Table.jsx
+++ code/05 Essentials Practice/00-mine/01-starting-project/src/components/Table.jsx
@@ -1,8 +1,40 @@
-import { calculateInvestmentResults } from "../util/investment";
-
+import { calculateInvestmentResults, formatter } from "../util/investment";
 export default function Table({ userInput }) {
-  
-    const results = calculateInvestmentResults(userInput);
-    console.log(results)
-  return <p></p>;
+  const results = calculateInvestmentResults(userInput);
+  const initialInvestment =
+    results[0].valueEndOfYear -
+    results[0].interest -
+    results[0].annualInvestment;
+
+  const result = results.map((yearData) => {
+    const totalInterest =
+      yearData.valueEndOfYear -
+      yearData.annualInvestment * yearData.year -
+      initialInvestment;
+    const totalAmountInvested = yearData.valueEndOfYear - totalInterest;
+    return (
+      <tr key={yearData.year}>
+        <td>{yearData.year}</td>
+        <td>{formatter.format(yearData.valueEndOfYear)}</td>
+        <td>{formatter.format(yearData.interest)}</td>
+        <td>{formatter.format(totalInterest)}</td>
+        <td>{formatter.format(totalAmountInvested)}</td>
+      </tr>
+    );
+  });
+
+  return (
+    <table id="result">
+      <thead>
+        <tr>
+          <th>Year</th>
+          <th>Investment Value</th>
+          <th>Interest (Year)</th>
+          <th>Total Interest</th>
+          <th>Invested Capital</th>
+        </tr>
+      </thead>
+        <tbody>{result}</tbody>
+    </table>
+  );
 }
